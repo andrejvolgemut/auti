ec0155fba85eee76eb392813e6bd4b4424f67538
Andrej Volgemut <andrej.volgemut@skole.hr>
2017-02-14 18:50:02 +0100

    Izmjenili smo naziv iznad forme.

--- brands_new.php
+++ brands_new.php
@@ -39,7 +39,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Form Validation</h3>
+                <h3>Proizvođači</h3>
               </div>
 
               <div class="title_rihaaaaaaaaaaaaaaaaaaaaaaaaght">
@@ -59,29 +59,28 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_content">
-                  <h1>Unos proizvođača</h1>
+                  <h1>Unos</h1>
                   
-                  <form action="brands_create.php" method="post" class="form-horizontal form-label-left">
-                  <div class="form-group">
+                <form action="brands_create.php" method="post" class="form-horizontal form-label-left">
+                <div class="form-group">
                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Ime proizvođača:</label>
                     <div class="col-md-6 col-sm-6 col-xs-12">
                       <input type="text" id="name" name="name" required="required" class="form-control col-md-7 col-xs-12">
                   </div>
                   </div>
 
-                  <div class="form-group">
+                 <div class="form-group">
                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="year_est">Datum osnutka:</label>
-                  <div class="col-md-6 col-sm-6 col-xs-12">
-                    <input type="text" id="year_est" name="year_est" required="required" class="form-control col-md-7 col-xs-12">
-                    
-                 <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                 <input type="text" id="year_est" name="year_est" required="required" class="form-control col-md-7 col-xs-12">
-                </div>
-                </div>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="text" id="year_est" name="year_est" required="required" class="form-control col-md-7 col-xs-12">
+                   
+                  </div>
+                  </div>
                 <div class="form-group">
-                  <div class="col-md-6 col-sm-6 col-xs-12>
+                  <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="submit" value="Unesi" class="btn btn sucess">
                   </div>
+                 </div>
                  
                     
                   </div>
